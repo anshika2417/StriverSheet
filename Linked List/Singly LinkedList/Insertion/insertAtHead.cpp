72294aa9ab904a4466c9367b60d8647622402915
anshika2417 <anshikakumari571@gmail.com>
2024-12-21 18:23:00 +0530

    Insertion at Head

---
+++ Linked List/Singly LinkedList/Insertion/insertAtHead.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+Node* insertAtHead(Node* &head,int val){
+    Node* temp=new Node(val);
+    temp->next=head;
+    head=temp;
+}
+
+void display(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL";
+}
+
+int main(){
+    Node* head=NULL;
+    insertAtHead(head,32);
+    insertAtHead(head,12);
+    insertAtHead(head,4);
+    insertAtHead(head,2);
+    display(head);
+}
