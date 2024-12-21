cf37e28afcd0bb6d24de1236448272af1404c234
anshika2417 <anshikakumari571@gmail.com>
2024-12-21 21:25:01 +0530

    Tail Deletion

---
+++ Linked List/Singly LinkedList/Deletion/TailDeletion.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+Node* insertAtHead(Node* &head,int val){
+    Node* temp=new Node(val);
+    temp->next=head;
+    head=temp;
+}
+
+Node* deleteHead(Node* &head){
+    if(head == NULL){
+        return head;
+    }
+
+    Node* temp=head;
+    head=head->next;
+    delete temp; 
+}
+
+Node* deleteTail(Node* head){
+    Node* temp=head;
+    Node* prev=NULL;
+    while(temp->next != NULL){
+        prev=temp;
+        temp=temp->next;
+    }
+    delete temp;
+    prev->next=NULL;
+}
+
+void display(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+    Node* head=NULL;
+    insertAtHead(head,32);
+    insertAtHead(head,12);
+    insertAtHead(head,4);
+    insertAtHead(head,2);
+    display(head);
+    deleteHead(head);
+    display(head);
+    deleteTail(head);
+    display(head);
+}
